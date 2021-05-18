d31eeaacfb82ccaba9955c5e8fc26ecee9a1b41e
Jason Chan <chanjaso1@green-parrot.ecs.vuw.ac.nz>
2021-05-18 17:54:07 +1200

    Defined all methods in task 1. Entires can be added and sorted by the latest.

--- src/main/java/nz/ac/wgtn/swen301/a3/server/LoggedEvent.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LoggedEvent.java
@@ -1,18 +1,33 @@
 package nz.ac.wgtn.swen301.a3.server;
 
-public class LoggedEvent {
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 
-    public String id, message, timestamp, thread, logger, errorDetails;
+public class LoggedEvent implements Comparable<LoggedEvent>{
 
-    public enum level {WARN, ERROR, DEBUG, INFO, ALL}
+    private String id, message, timestamp, thread, logger, level, errorDetails;
 
-    public LoggedEvent(String id, String message, String timestamp, String thread, String logger, String errorDetails) {
+
+    @Override
+    public int compareTo(LoggedEvent other) {
+        return other.getTimestamp().compareTo(this.getTimestamp());
+    }
+
+    public static ArrayList<String> levels = new ArrayList<>(Arrays.asList("ALL", "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL", "OFF"));
+
+
+    public LoggedEvent(String id, String message, String timestamp, String thread, String logger, String level, String errorDetails) {
         this.id = id;
         this.message = message;
         this.timestamp = timestamp;
         this.thread = thread;
         this.logger = logger;
         this.errorDetails = errorDetails;
+        this.level = level;
     }
 
     public String getId() {
@@ -23,8 +38,14 @@ public class LoggedEvent {
         return message;
     }
 
-    public String getTimestamp() {
-        return timestamp;
+    public Timestamp getTimestamp() {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+            Date parsedDate = dateFormat.parse(timestamp);
+            return new Timestamp(parsedDate.getTime());
+        }catch(Exception e){
+            return null;
+        }
     }
 
     public String getThread() {
@@ -34,6 +55,8 @@ public class LoggedEvent {
     public String getLogger() {
         return logger;
     }
+
+    public String getLevel() { return level; }
 
     public String getErrorDetails() {
         return errorDetails;
--- src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -1,6 +1,7 @@
 package nz.ac.wgtn.swen301.a3.server;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,21 +10,41 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
 
 public class LogsServlet extends HttpServlet {
 
-
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
     public LogsServlet() {
     }
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
         PrintWriter out = response.getWriter();
 
-        String levels = request.getParameter("level");
-        String limit = request.getParameter("limit");
+        String l = request.getParameter("limit");
+        String level = request.getParameter("level");
+        System.out.println(l);
+        System.out.println(level);
+        if(!check(level, l)) response.setStatus(400);
+        else {
+            int limit = Integer.parseInt(l);
 
+            List<LoggedEvent> results = filter(level);
+            Collections.sort(results);
+
+            if(limit < results.size()) results = results.subList(0, limit);
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            out.println(gson.toJson(results));
+            response.setStatus(200);
+        }
+        out.close();
     }
 
 
@@ -37,9 +58,38 @@ public class LogsServlet extends HttpServlet {
         }catch (IOException ignored){}
 
         resp.setContentType("application/json");
-        LoggedEvent event = new Gson().fromJson(JSONString.toString(), LoggedEvent.class);
-
+        LoggedEvent event = gson.fromJson(JSONString.toString(), LoggedEvent.class);
+        try {
+            assert LoggedEvent.levels.contains(event.getLevel());
+        }catch (AssertionError e) {
+            e.printStackTrace();
+            return;
+        }
         Persistency.DB.add(event);
+    }
+
+    public List<LoggedEvent> filter(String level){
+        List<LoggedEvent> results = new ArrayList<>();
+        for (LoggedEvent event : Persistency.DB) {
+            if(event.getLevel().equals(level) || level.equals("ALL")) results.add(event);
+        }
+
+        return results;
+
+    }
+    public boolean check(String level, String limit){
+        try{
+
+            assert LoggedEvent.levels.contains(level);
+          //  if(LoggedEvent.levels.contains(level)) throw new AssertionError("Level: " + level + " is not a level");
+            int lim = Integer.parseInt(limit);
+            assert lim >= 0 && lim < Persistency.DB.size();
+
+        } catch (AssertionError e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     @Override
